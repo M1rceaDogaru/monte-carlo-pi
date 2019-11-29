656d44c198ead39929984626c85e2f18adddeb25
Mircea Dogaru <mircea_dogaru@channelsight.com>
2019-11-29 11:05:36 +0200

    Add project files

---
+++ ComputePi.qs
@@ -0,0 +1,25 @@
+﻿namespace Quantum.MonteCarlo
+{
+    open Microsoft.Quantum.Intrinsic;
+    open Microsoft.Quantum.Canon;
+	open Microsoft.Quantum.Math;
+	open Microsoft.Quantum.Convert;
+    
+    operation ComputePi (iterations: Int, computePrecision: Int) : Double {
+		let refRadius = 0.99999999;
+		mutable insideRefCount = 0.0;
+
+        for (i in 1..iterations) {
+			let x = RandomReal(computePrecision);
+			let y = RandomReal(computePrecision);
+
+			let refX = Sqrt(refRadius - PowD(y, 2.0));
+			let refY = Sqrt(refRadius - PowD(x, 2.0));
+			if (x < refX and y < refY) {
+				set insideRefCount += 1.0;
+			}
+		}
+
+		return 4.0 * insideRefCount / IntAsDouble(iterations);
+    }
+}
---
+++ Driver.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Quantum.Simulation.Simulators;
+
+namespace Quantum.MonteCarlo
+{
+    class Driver
+    {
+        static async Task Main(string[] args)
+        {
+            using (var qsim = new QuantumSimulator())
+            {
+                var runs = 10;
+                Console.WriteLine($"Please wait while we compute PI for {runs} runs...");
+
+                for (var i = 0; i < runs; i++)
+                {
+                    var pi = await ComputePi.Run(qsim, 1000000, 35);
+                    Console.WriteLine($"Run {i + 1} : {pi}");
+                }
+
+                Console.WriteLine("Done");
+            }
+        }
+    }
+}
